8cd666443ad301bef2772288983222e08ac906ef
mooncommit <mooncommit@users.noreply.github.com>
2026-03-28 03:22:46 +0900

    STEP2, String.format() 활용

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -8,37 +8,38 @@ public class CommerceSystem {
     private List<Product> productList;
     private Scanner scanner;
 
+
     // 생성자
-    public CommerceSystem(List<Product> productList, Scanner scanner) {
+    public CommerceSystem (List<Product> productList, Scanner scanner) {
         this.productList = productList;
         this.scanner = scanner;
     }
 
     // 기능
-
     public void start() {
-            while (true) {
+        // 반복문으로 ProductList 출력
+        while (true) {
+            int number = 0;
+            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ] ");
+            for (Product products : productList) {
+                number++;
+//                System.out.println(number + ". " + products.getName() + "  |  " + products.getPrice() + "  |  " + products.getExplain());
+                // String.format("형식", 값) 활용
+                System.out.println(String.format("%d. %s | %,d원 | %s", number, products.getName(), products.getPrice(), products.getExplain()));
+            }
+            System.out.println("0. 종료          | 프로그램 종료");
+            System.out.println("--------------------------------");
+            System.out.print("입력 : ");
+            int exit = scanner.nextInt();
+            if (exit == 0) {
+                System.out.println("커머스 플랫폼을 종료합니다.");
 
-        int count = 1;
-        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
-        for (Product product : productList) {
-            System.out.println(count + ". " + product.getName() + "   |     " + product.getPrice() + "원" + "    |    " + product.getExplain() + "  |" + product.getCount());
-            count++;
-        }
+                break;
+            }
 
-        System.out.println("0. 종료            | 프로그램 종료");
-
-        System.out.print("입력 : ");
-
-        int exit = scanner.nextInt();
-        if (exit == 0) {
-            System.out.println("------------------");
-            System.out.println("커머스 플랫폼을 종료합니다.");
-            break;
         }
     }
 
-    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Product> productList = new ArrayList<>();
 
         // 객체화 (인스턴스화)
         Product galaxy = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 0);
@@ -12,38 +13,13 @@ public class Main {
         Product macbook = new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 100);
         Product airpods = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 40);
 
-        // 리스트 만들기
-        List<Product> productList = new ArrayList<>();
         productList.add(galaxy);
         productList.add(iphone);
         productList.add(macbook);
         productList.add(airpods);
 
-
-        // 반복문으로 ProductList 출력
-        while (true) {
-            int number = 0;
-            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ] ");
-            for (Product products : productList) {
-                number++;
-                System.out.println(number + ". " + products.getName() + "  |  " + products.getPrice() + "  |  " + products.getExplain());
-            }
-            System.out.println("0. 종료          | 프로그램 종료");
-            System.out.println("--------------------------------");
-            System.out.print("입력 : ");
-            int exit = scanner.nextInt();
-            if (exit == 0) {
-                System.out.println("커머스 플랫폼을 종료합니다.");
-
-                break;
-            }
-
-        }
-
-
-
-
-
+        CommerceSystem commerceSystem = new CommerceSystem(productList, scanner);
+        commerceSystem.start();
     }
 
 }
