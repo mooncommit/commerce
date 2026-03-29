0f63da9e4c58b29349947db2e50cbdf254cb7695
mooncommit <mooncommit@users.noreply.github.com>
2026-03-29 23:18:31 +0900

    STEP1 List.of 사용

--- src/Main.java
+++ src/Main.java
@@ -6,46 +6,35 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // 객체화 (인스턴스화)
         // Product 전자제품 객체 만들기
-        Category galaxy = new Category("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 0);
-        Category iphone = new Category("iPhone 16", 1350000, "Apple의 최신 스마트폰", 10);
-        Category macbook = new Category("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 100);
-        Category airpods = new Category("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 40);
+        Product galaxy = new Product("Galaxy S25", 1200000, "최신 안드로이드 스마트폰", 0);
+        Product iphone = new Product("iPhone 16", 1350000, "Apple의 최신 스마트폰", 10);
+        Product macbook = new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북", 20);
+        Product airpods = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰", 100);
 
-        // 의류 객체 만들기
-        Category aloBag = new Category("Alo Bag", 67000, "여행갈 때 너무너무 예쁜 가방", 50);
-        Category adidasShoes = new Category("Adidas Tokyo Core", 84000, "아이다스의 굿 아이템 신발", 20);
-        Category casioClock = new Category("카시오 LTP-V007L-7B1", 30000, "가성비 완전 혁명 시계", 1000);
-        Category adidasTop = new Category("Adidas Dot Track Top", 118000, "땡땡이가 매력점인 아우터", 2);
+        // 전자제품 리스트
+        // List.of 사용 : 괄호 안의 값들로 리스트 만들기
+        List<Product> electronicsList = new ArrayList<>(List.of(galaxy, iphone, macbook, airpods));
 
+        // 반복문 활용
+        while (true) {
+            int count = 1;
+            System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+            for (Product product : electronicsList) {
+                System.out.println(String.format("%d. %s | %,d원 | %s", count, product.getName(), product.getPrice(), product.getExplain()));
+                count ++;
+            }
+            System.out.println("0. 종료        | 프로그램 종료");
+            System.out.print("입력 : ");
+            int exit = scanner.nextInt();
+            if (exit == 0) {
+                System.out.println("커머스 플랫폼을 종료합니다.");
+                break;
+            }
 
-        // CommerceSystem 객체화
-        CommerceSystem commerceSystem = new CommerceSystem(, scanner);
-
-        // 전자제품 List
-        List<Category> electronicsList = new ArrayList<>();
-        electronicsList.add(galaxy);
-        electronicsList.add(iphone);
-        electronicsList.add(macbook);
-        electronicsList.add(airpods);
-
-        // 의류 List
-        List<>
-
-
-        // Category 객체화
-        Category electronics = new Category(electronicsList, "전자제품");
-        Category clothes = new Category(productList, "의류");
-
-        electronics.getProductList();
+        }
 
 
-
-
-
-        // 프로그램 시작
-        commerceSystem.start();
     }
 
 }
--- src/Product.java
+++ src/Product.java
@@ -1,35 +1,31 @@
 public class Product {
-//    상품명, 가격, 설명, 재고수량
-    // 속성 (필드)
+//    상품명, 가격, 설명, 재고수량 필드
+
     private String name;
     private int price;
     private String explain;
-    private int quantity;
+    private int pics;
 
-    // 생성자
-    public Product (String name, int price, String explain, int quantity) {
+    public Product (String name, int price, String explain, int pics) {
         this.name = name;
         this.price = price;
         this.explain = explain;
-        this.quantity = quantity;
+        this.pics = pics;
     }
 
-    // 기능
-    // 이 객체의 name 필드의 저장된 값을 반환해주는 메서드
     public String  getName() {
         return name;
     }
 
     public int getPrice() {
-        return price;
+        return  price;
     }
-
     public String getExplain() {
         return explain;
     }
 
-    public int getQuantity() {
-        return quantity;
+    public int getPics() {
+        return pics;
     }
 
 
