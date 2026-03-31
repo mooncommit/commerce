c777a26dc51912bda741204286dd998e9388a5f0
mooncommit <mooncommit@users.noreply.github.com>
2026-03-31 20:29:51 +0900

    STEP3 출력 코드 정리

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -1,3 +1,4 @@
+import javax.smartcardio.Card;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,6 +8,8 @@ public class CommerceSystem {
     // 속성
     private List<Category> categoryList;
     private Scanner scanner;
+    private Category selectedCategory;
+    private Product selectedProduct;
 
     // 생성자
     public CommerceSystem(List<Category> categoryList, Scanner scanner) {
@@ -18,7 +21,6 @@ public class CommerceSystem {
     // 프로그램 시작 기능
     public void start() {
         while (true) {
-
             // 1. 카테고리 목록 출력
             System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
             int categoryCount = 1;
@@ -26,48 +28,46 @@ public class CommerceSystem {
                 System.out.println(categoryCount + ". " + category.getCategoryName());
                 categoryCount++;
             }
-
             // 2. 입력값 받기
             System.out.println("0. 종료      | 프로그램 종료");
             System.out.print("입력 : ");
-            int categoryNumber = scanner.nextInt();
+            int inputCategory = scanner.nextInt();
 
             // 3. 카테고리 선택
-            if (categoryNumber == 0) {
+            // 예외처리 먼저 하기 (0(종료),1(카테고리 선택),2(카테고리 선택), 3(다시 입력))
+            if (inputCategory == 0) {
                 System.out.println("커머스 플랫폼을 종료합니다.");
                 break;
-            } else if (categoryNumber >= 0 && categoryList.size() >= categoryNumber) {
-                Category selectCategory = categoryList.get(categoryNumber - 1);
-                List<Product> productList = selectCategory.getProductList();
-
-                // 4. 상품 목록 출력
-                System.out.println("[ " + selectCategory.getCategoryName() + " 카테고리 ]");
-                int productCount = 1;
-                for (Product product : productList) {
-                    System.out.println(String.format("%d. %s | %,d원 | %s",
-                            productCount, product.getProductName(), product.getProductPrice(), product.getProductExplain()));
-                    productCount++;
-                }
-
-                // 5. 입력값 받기
-                System.out.println("0. 뒤로가기");
-                System.out.print("입력 : ");
-                int productNumber = scanner.nextInt();
-
-                // 6. 상품 선택
-                if (productNumber == 0) {
-                    continue;
-                } else if (productNumber >= 0 && productList.size() >= productNumber) {
-                    Product product = productList.get(productNumber - 1);
-                    System.out.println(String.format("선택한 상품 : %s | %,d원 | %s | 재고 : %,d개",
-                            product.getProductName(), product.getProductPrice(),
-                            product.getProductExplain(), product.getProductQuantity()));
-                }
+            } else if (0 < inputCategory && inputCategory < categoryList.size()) {
+                selectedCategory = categoryList.get(inputCategory - 1);
             } else {
-                System.out.println("올바른 번호를 입력해주세요");
+                System.out.println("올바른 숫자를 입력해주세요");
                 continue;
             }
-        }
 
+            // 4. 상품목록 출력
+            List<Product> productList = selectedCategory.getProductList();
+            int productCount = 1;
+            for (Product product : productList) {
+                System.out.println(String.format("%d. %s | %,d | %s",
+                        productCount, product.getProductName(), product.getProductPrice(), product.getProductExplain()));
+            }
+
+            // 5. 입력값 받기
+            System.out.println("0. 뒤로가기");
+            System.out.print("입력 : ");
+            int inputProduct = scanner.nextInt();
+
+            // 6. 상품 선택
+            if (inputProduct == 0) {
+                continue;
+            } else if (inputProduct >= 0 && productList.size() >= inputProduct) {
+                selectedProduct = productList.get(inputProduct - 1);
+                System.out.println(String.format("선택한 상품 : %s | %,d | %s | 재고 : %,d개",
+                        selectedProduct.getProductName(), selectedProduct.getProductPrice(),
+                        selectedProduct.getProductExplain(), selectedProduct.getProductQuantity()));
+            }
+        }
     }
 }
+// 분리를 어떤 기준으로 분리할 수 있을까 계획하기!
