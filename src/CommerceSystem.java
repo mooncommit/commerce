a72d2532837f30c2e2a6a9406b625c6b84aec725
mooncommit <mooncommit@users.noreply.github.com>
2026-04-01 16:02:08 +0900

    STEP3 최종완성

--- src/CommerceSystem.java
+++ src/CommerceSystem.java
@@ -20,16 +20,16 @@ public class CommerceSystem {
     private void displayCategoryList() {
         // 1. 카테고리 목록 출력
         System.out.println("[ 실시간 커머스 플랫폼 메인 ]");
-        int categoryCount = 1;
+        int countCategory = 1;
         for (Category category : categoryList) {
-            System.out.println(categoryCount + ". " + category.getCategoryName());
-            categoryCount++;
+            System.out.println(countCategory + ". " + category.getCategoryName());
+            countCategory++;
         }
-        System.out.println("0. 종료      | 프로그램 종료");
     }
 
     private int returnCategoryNumber() {
         // 2. 입력값 받기
+        System.out.println("0. 종료      | 프로그램 종료");
         System.out.print("입력 : ");
         int inputCategory = scanner.nextInt();
         return inputCategory;
@@ -37,14 +37,12 @@ public class CommerceSystem {
 
     private String selectCategory(int inputCategory) {
         // 3. 카테고리 선택
-        // 예외처리 먼저 하기 (0(종료),1(카테고리 선택),2(카테고리 선택), 3(다시 입력))
         if (inputCategory == 0) {
             System.out.println("커머스 플랫폼을 종료합니다.");
             return "break";
         } else if (0 < inputCategory && inputCategory <= categoryList.size()) {
             selectedCategory = categoryList.get(inputCategory - 1);
-            System.out.println("----------------------------------qm");
-            return "product";
+            return "null";
         } else {
             System.out.println("올바른 숫자를 입력해주세요");
             return "continue";
@@ -52,38 +50,40 @@ public class CommerceSystem {
     }
 
     private void displayProductList() {
-        // 4. 상품목록 출력
         List<Product> productList = selectedCategory.getProductList();
-        int productCount = 1;
+        int countProduct = 1;
         for (Product product : productList) {
             System.out.println(String.format("%d. %s | %,d | %s",
-                    productCount, product.getProductName(), product.getProductPrice(), product.getProductExplain()));
-            productCount++;
+                    countProduct, product.getProductName(), product.getProductPrice(), product.getProductExplain()));
+            countProduct++;
         }
     }
 
     private int returnProductNumber() {
-        // 5. 입력값 받기
         System.out.println("0. 뒤로가기");
         System.out.print("입력 : ");
         int inputProduct = scanner.nextInt();
         return inputProduct;
     }
 
-    private String selectProduct(int inputProduct, List<Product> productList) {
+    private String selectProduct(int inputProduct) {
         // 6. 상품 선택
         // 예외처리 먼저 하기 (0(종료),1(상품목록 선택),2(상품목록 선택), 3(상품목록 선택) 4(다시 입력))
+        List<Product> productList = selectedCategory.getProductList();
+
         if (inputProduct == 0) {
-            return "continue;";
+            return "continue";
         } else if (0 < inputProduct && inputProduct <= productList.size()) {
             selectedProduct = productList.get(inputProduct - 1);
-            return "product";
+            return "null";
         } else {
             System.out.println("올바른 숫자를 입력해주세요");
             return "continue";
         }
     }
+
     private void displayProduct() {
+        // 7. 상품 출력
         System.out.println(String.format("선택한 상품 : %s | %,d | %s | 재고 : %,d개",
                 selectedProduct.getProductName(), selectedProduct.getProductPrice(),
                 selectedProduct.getProductExplain(), selectedProduct.getProductQuantity()));
@@ -96,17 +96,17 @@ public class CommerceSystem {
             // displayCategoryList
             // 1. 카테고리 목록 출력
             displayCategoryList();
+
             // returnCategoryNumber
             // 2. 입력값 받기
-            int inputCategory = returnCategoryNumber(); // 여기 복습
+            int inputCategory = returnCategoryNumber();
 
             // selectCategory
             // 3. 카테고리 선택
-            String categorySignal = selectCategory(inputCategory);
-            // signal 보냈을 때 "break" 이면 break, "continue" = continue, "product" 놉
-            if (categorySignal.equals("break")) {
+            String signalCategory = selectCategory(inputCategory);
+            if (signalCategory.equals("break")) {
                 break;
-            } else if (categorySignal.equals("continue")) {
+            } else if (signalCategory.equals("continue")) {
                 continue;
             }
 
@@ -119,12 +119,15 @@ public class CommerceSystem {
             int inputProduct = returnProductNumber();
 
             // selectProduct
+            // 걷기 1회차에서...
+            // 인수!!
+            // 매개변수
             // 6. 상품 선택
-            List<Product> productList = selectedCategory.getProductList();
-            String  productSignal = selectProduct(inputProduct, productList);
-            if (productSignal.equals("break")) {
+            // 예외처리 먼저 하기 (0(종료),1(상품목록 선택),2(상품목록 선택), 3(상품목록 선택) 4(다시 입력))
+            String signalProduct = selectProduct(inputProduct);
+            if (signalProduct.equals("break")) {
                 break;
-            } else if (productSignal.equals("continue")) {
+            } else if (signalProduct.equals("continue")) {
                 continue;
             }
 
@@ -134,6 +137,3 @@ public class CommerceSystem {
         }
     }
 }
-// 분리를 어떤 기준으로 분리할 수 있을까 계획하기!
-//
-// 카테고리 / 상품목록
